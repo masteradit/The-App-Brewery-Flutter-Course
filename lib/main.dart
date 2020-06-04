46ecbbad889a153f1a78f4dac815f0b1a1acea0a
Master Adit <aditluhadia@gmail.com>
2020-06-04 22:08:27 +0530

    Incorporating Custom Fonts in Your Flutter App
    Section 6: MiCard - How to Build Beautiful UIs with Flutter Widgets

--- lib/main.dart
+++ lib/main.dart
@@ -20,8 +20,19 @@ class MyApp extends StatelessWidget {
               Text(
                 'Adit Luhadia',
                 style: TextStyle(
+                  fontFamily: 'Pacifico',
                   fontSize: 40.0,
                   color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                'FLUTTER DEVELOPER',
+                style: TextStyle(
+                  fontFamily: 'Source Sans Pro',
+                  color: Colors.teal.shade100,
+                  fontSize: 20.0,
+                  letterSpacing: 2.5,
                   fontWeight: FontWeight.bold,
                 ),
               ),
