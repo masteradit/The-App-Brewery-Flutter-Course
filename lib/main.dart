21766563ea1c58e46989eaa9dc1055e4183711c8
Master Adit <aditluhadia@gmail.com>
2020-06-06 18:51:25 +0530

    Step 5 - Randomise it
    Section 8: Boss Level Challenge 1 - Magic 8 Ball

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(
@@ -26,14 +28,17 @@ class Ball extends StatefulWidget {
 }
 
 class _BallState extends State<Ball> {
+  int ballNumber = 1;
+
   @override
   Widget build(BuildContext context) {
     return Center(
       child: FlatButton(
         onPressed: () {
-          print('I got clicked');
+          ballNumber = Random().nextInt(5);
+          print(ballNumber);
         },
-        child: Image.asset('images/ball1.png'),
+        child: Image.asset('images/ball$ballNumber.png'),
       ),
     );
   }
