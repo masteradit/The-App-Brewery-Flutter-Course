b9493c6968c4dac059d0cd96f90c5324c911a657
Master Adit <aditluhadia@gmail.com>
2020-07-25 02:20:29 +0530

    The Flutter Slider Widget

--- lib/icon_content.dart
+++ lib/icon_content.dart
@@ -1,10 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
-const labelTextStyle = TextStyle(
-  fontSize: 18.0,
-  color: Color(0xFF8D8E98),
-);
+import 'constants.dart';
 
 class IconContent extends StatelessWidget {
   IconContent({this.icon, this.label});
@@ -26,7 +23,7 @@ class IconContent extends StatelessWidget {
         ),
         Text(
           label,
-          style: labelTextStyle,
+          style: kLabelTextStyle,
         ),
       ],
     );
