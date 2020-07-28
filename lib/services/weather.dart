a7766f2c5b325b17f9d1575c220c31cab421260a
Master Adit <aditluhadia@gmail.com>
2020-07-28 13:22:56 +0530

    Passing Data Backwards Through the Navigation Stack

--- lib/services/weather.dart
+++ lib/services/weather.dart
@@ -5,6 +5,14 @@ const apiKey = '73fa3d12f54899e82a5ca6ff974c2727';
 const openWeatherMapURL = 'https://api.openweathermap.org/data/2.5/weather';
 
 class WeatherModel {
+  Future<dynamic> getCityWeather(String cityName) async {
+    NetworkHelper networkHelper = NetworkHelper(
+        '$openWeatherMapURL?q=$cityName&appid=$apiKey&units=metric');
+
+    var weatherData = await networkHelper.getData();
+    return weatherData;
+  }
+
   Future<dynamic> getLocationWeather() async {
     Location location = Location();
     await location.getCurrentLocation();
