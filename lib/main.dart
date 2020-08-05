d8161246985eee9d5a58159efa9e846d4803fecc
Master Adit <aditluhadia@gmail.com>
2020-08-05 19:47:53 +0530

    Named Routes Challenge

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
 
+import 'screens/chat_screen.dart';
+import 'screens/login_screen.dart';
+import 'screens/registration_screen.dart';
+import 'screens/welcome_screen.dart';
+import 'screens/welcome_screen.dart';
 import 'screens/welcome_screen.dart';
 
 void main() => runApp(FlashChat());
@@ -13,7 +18,13 @@ class FlashChat extends StatelessWidget {
           bodyText2: TextStyle(color: Colors.black54),
         ),
       ),
-      home: WelcomeScreen(),
+      initialRoute: WelcomeScreen.id,
+      routes: {
+        WelcomeScreen.id: (context) => WelcomeScreen(),
+        '/login_screen': (context) => LoginScreen(),
+        '/registration_screen': (context) => RegistrationScreen(),
+        '/chat_screen': (context) => ChatScreen(),
+      },
     );
   }
 }
