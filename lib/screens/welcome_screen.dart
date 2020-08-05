38b1be266e0438a19db455762b5b304cb47afb83
Master Adit <aditluhadia@gmail.com>
2020-08-05 19:55:24 +0530

    Refactor Routes with the Static Const

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+import 'login_screen.dart';
+import 'registration_screen.dart';
+
 class WelcomeScreen extends StatefulWidget {
   static const String id = '/welcome_screen';
 
@@ -43,9 +46,7 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
                 color: Colors.lightBlueAccent,
                 borderRadius: BorderRadius.circular(30.0),
                 child: MaterialButton(
-                  onPressed: () {
-                    //Go to login screen.
-                  },
+                  onPressed: () => Navigator.pushNamed(context, LoginScreen.id),
                   minWidth: 200.0,
                   height: 42.0,
                   child: Text(
@@ -61,9 +62,8 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
                 borderRadius: BorderRadius.circular(30.0),
                 elevation: 5.0,
                 child: MaterialButton(
-                  onPressed: () {
-                    //Go to registration screen.
-                  },
+                  onPressed: () =>
+                      Navigator.pushNamed(context, RegistrationScreen.id),
                   minWidth: 200.0,
                   height: 42.0,
                   child: Text(
