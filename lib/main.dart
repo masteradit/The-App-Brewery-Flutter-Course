48136392c7348286931e877e786733e4c5c029b9
Master Adit <aditluhadia@gmail.com>
2020-08-05 19:49:38 +0530

    Removed duplicate imports

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,6 @@ import 'screens/chat_screen.dart';
 import 'screens/login_screen.dart';
 import 'screens/registration_screen.dart';
 import 'screens/welcome_screen.dart';
-import 'screens/welcome_screen.dart';
-import 'screens/welcome_screen.dart';
 
 void main() => runApp(FlashChat());
 
