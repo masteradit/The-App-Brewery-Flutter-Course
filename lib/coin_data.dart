d58890c974ecbf200cc58487f600a2dadf5b4da8
Master Adit <aditluhadia@gmail.com>
2020-08-05 19:06:31 +0530

    TODO 5: Return a Map of the results instead of a single value.

--- lib/coin_data.dart
+++ lib/coin_data.dart
@@ -37,17 +37,19 @@ const apiKey = '839EB29A-5B71-4EFC-BB6C-5E4D17CD2DFC';
 
 class CoinData {
   Future getCoinData(String selectedCurrency) async {
+    Map<String, String> cryptoPrices = {};
     for (String crypto in cryptoList) {
       String requestURL = '$coinAPIURL/BTC/$selectedCurrency?apikey=$apiKey';
       http.Response response = await http.get(requestURL);
       if (response.statusCode == 200) {
         var decodedData = jsonDecode(response.body);
         double lastPrice = decodedData['rate'];
+        cryptoPrices[crypto] = lastPrice.toStringAsFixed(0);
       } else {
         print(response.statusCode);
         throw 'Problem with the get request';
       }
     }
-    //TODO 5: Return a Map of the results instead of a single value.
+    return cryptoPrices;
   }
 }
